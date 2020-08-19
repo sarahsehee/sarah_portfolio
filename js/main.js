4ffdd332198388f120365a9e602e782a103c56fc
Sehee Lim <sarahsehee.lim@gmail.com>
2020-08-19 15:24:37 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -5,23 +5,27 @@ function show(section) {
     /* Button Ids */
     const ButtonProject = document.getElementById("lk_projects");
     const ButtonResume = document.getElementById("lk_resume");
-    /* Right Div*/
+    /* Right Div
     const Right = document.getElementById("right_bottom");
-
+*/
 
 
     if (section === 'resume') {
         Projects.style.display="none";
         Resume.style.display="block";
-        ButtonResume.style.color="var(--color-softpink)";
         ButtonProject.style.color="white";
+        ButtonResume.style.color="var(--color-softpink)";
+        /*
         Right.style.backgroundColor="var(--color-softpink)";
+        */
     }
     else if (section === 'projects') {
         Projects.style.display="block";
         Resume.style.display="none";
         ButtonProject.style.color="#D9FF6F";
         ButtonResume.style.color="white";
+        /*
         Right.style.backgroundColor="var(--color-highlight)";
+        */
     }
 }
