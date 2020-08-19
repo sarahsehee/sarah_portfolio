0b28ffc077441bbe7c94ee7cf73b9ff5142e563b
Sehee Lim <sarahsehee.lim@gmail.com>
2020-08-19 13:48:33 +0900

    * changed layout * changed color

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-console.log("hello");
-
-
-
 function show(section) {
+    /* display contents*/
     const Projects = document.getElementById("projects");
     const Resume = document.getElementById("resume");
+    /* Button Ids */
+    const ButtonProject = document.getElementById("lk_projects");
+    const ButtonResume = document.getElementById("lk_resume");
+    /* Right Div*/
+    const Right = document.getElementById("right_bottom");
+
     if (section === 'resume') {
         Projects.style.display="none";
         Resume.style.display="block";
+        ButtonResume.style.color="var(--color-softpink)";
+        ButtonProject.style.color="white";
+        Right.style.backgroundColor="var(--color-softpink)";
     }
     else if (section === 'projects') {
         Projects.style.display="block";
         Resume.style.display="none";
+        ButtonProject.style.color="#D9FF6F";
+        ButtonResume.style.color="white";
+        Right.style.backgroundColor="var(--color-highlight)";
     }
-    console.log(section);
 }
