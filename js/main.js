8694ce3f1a7a4c9c1f3fe98741ece39e74c4921b
Sehee Lim <sarahsehee.lim@gmail.com>
2020-09-21 00:23:56 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
-function show(section) {
-    /* display contents*/
-    const Projects = document.getElementById("projects");
-    const Resume = document.getElementById("resume");
-    /* Button Ids */
-    const ButtonProject = document.getElementById("lk_projects");
-    const ButtonResume = document.getElementById("lk_resume");
+$(document).ready(function() {
+  const ButtonProject = document.getElementById("lk_projects");
+  ButtonProject.style.color = "var(--color-highlight)";
+});
 
-    if (section === 'resume') {
-        Projects.style.display="none";
-        Resume.style.display="block";
-        ButtonProject.style.color="white";
-        ButtonResume.style.color="var(--color-coral)";
-    }
-    else if (section === 'projects') {
-        Projects.style.display="block";
-        Resume.style.display="none";
-        ButtonProject.style.color="var(--color-coral)";
-        ButtonResume.style.color="white";
-    }
+function show(section) {
+  /* display contents*/
+  const Projects = document.getElementById("projects");
+  const Resume = document.getElementById("resume");
+  /* Button Ids */
+  const ButtonProject = document.getElementById("lk_projects");
+  const ButtonResume = document.getElementById("lk_resume");
+
+  if (section === "resume") {
+    Projects.style.display = "none";
+    Resume.style.display = "block";
+    ButtonProject.style.color = "white";
+    ButtonResume.style.color = "var(--color-highlight)";
+  } else if (section === "projects") {
+    Projects.style.display = "block";
+    Resume.style.display = "none";
+    ButtonProject.style.color = "var(--color-highlight)";
+    ButtonResume.style.color = "white";
+  }
 }
